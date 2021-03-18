3e39cccb59d820602bc7b0065ba1ef0a6c032438
bc-dipesh <bcdipeshwork@gmail.com>
2021-03-18 09:30:15 +0545

    FEAT: Add Previous Button
    Add a previous button to navigate back to the main activity from second activity

--- app/src/main/java/com/example/fragments/SecondActivity.java
+++ app/src/main/java/com/example/fragments/SecondActivity.java
@@ -1,8 +1,10 @@
 package com.example.fragments;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+    public void launchMainActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
